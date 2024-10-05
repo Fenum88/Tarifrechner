c5b547cae384d039b52285fdae57d523da35ed4f
Rainer Schmidt <fenum@hotmail.de>
2024-10-05 10:14:44 +0200

    DAV 2004

--- Ausgabe.xaml.cs
+++ Ausgabe.xaml.cs
@@ -29,7 +29,7 @@ namespace Tarifrechner
     }
     public class GesamtOutput
     {
-        public int m {  get; set; }
+        public int m { get; set; }
         public int alter { get; set; }
         public double qx { get; set; }
         public double zins { get; set; }
@@ -51,6 +51,16 @@ namespace Tarifrechner
         public double mVx_a_gez { get; set; }
         //Bilanzielle
         public double mVx_B { get; set; }
+        //Inkassokosten
+        public double beta_kosten { get; set; }
+        //Verwaltungskosten
+        public double gamma_kosten { get; set; }
+        //Rentenbezugskosten
+        public double delta_kosten { get; set; }
+        //alpha_gamma_kosten
+        public double alpha_gamma_kosten { get; set; }
+        //Gesamtkosten
+        public double gesamtkosten { get; set; }
 
 
 
@@ -62,11 +72,12 @@ namespace Tarifrechner
     {
         public List<Tafel> TafelListe { get; set; }
         public List<GesamtOutput> GesamtOutput { get; set; }
+        public DAV2004 DAV2004 { get; set; }
         public Ausgabe()
         {
             InitializeComponent();
-            TafelListe = LeseSterbetafel("DAV2008.csv");
             GesamtOutput = new();
+            DAV2004 = new();
             DataContext = this;  // Bindet die Daten an das XAML
         }
 
@@ -77,11 +88,11 @@ namespace Tarifrechner
             string dateiPfad = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", dateiName);
             string dateiZins = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Zinsstruktur.csv");
 
-            var datenpunkte = new List<Tafel>(); 
+            var datenpunkte = new List<Tafel>();
             var zeilenSterbetafel = File.ReadAllLines(dateiPfad);
             var zeilenZinsStruktur = File.ReadAllLines(dateiZins);
 
-            for (int index =1 ; index < zeilenSterbetafel.Length; index++)
+            for (int index = 1; index < zeilenSterbetafel.Length; index++)
             {
                 var zeile = zeilenSterbetafel[index];
                 var spalten = zeile.Split(';');
@@ -98,14 +109,17 @@ namespace Tarifrechner
 
         public void Berechne(Vertrag vertrag)
         {
+            TafelListe = LeseSterbetafel("DAV2008.csv");
+            DAV2004 = new DAV2004(5.0, 10.0, 2024, vertrag.Vertragsteil.ea, vertrag.Vertragsteil.n, TafelListe);
             GesamtOutput = new();
             BerechneQx(vertrag);
 
             Nettobeitrag.Text = BerechneNettobeitrag(vertrag).ToString();
             Bruttobeitrag.Text = BerechneBruttobeitrag(vertrag).ToString();
             Zillmerprämie.Text = BerechnZillmerbeitrag(vertrag).ToString();
-            (double alpha_z1,double alpha_gamma1) = BerechneAbschlusskosten(vertrag);
+            (double alpha_z1, double alpha_gamma1) = BerechneAbschlusskosten(vertrag);
             (alpha_z.Text, alpha_gamma.Text) = (alpha_z1.ToString(), alpha_gamma1.ToString());
+            BerechneKostenverlauf(vertrag);
             BerechneDeckungsrueckstellungen(vertrag);
             GesamtAusgabeBox.ItemsSource = null;
             GesamtAusgabeBox.ItemsSource = GesamtOutput;
@@ -116,7 +130,7 @@ namespace Tarifrechner
             // Berechnung fuer Todesfallleistung
             double beitrag = GesamtOutput[0].lbw_tod * vertrag.Vertragsteil.leistung2 / GesamtOutput[0].lbw_praemie;
             // Berechnung fuer Erlebensfallleistung
-            beitrag+= GesamtOutput[0].lbw_erleben * vertrag.Vertragsteil.leistung1 / GesamtOutput[0].lbw_praemie;
+            beitrag += GesamtOutput[0].lbw_erleben * vertrag.Vertragsteil.leistung1 / GesamtOutput[0].lbw_praemie;
             return beitrag;
         }
 
@@ -138,16 +152,16 @@ namespace Tarifrechner
             double gamma = vertrag.Rechnungsgrundlage.gamma;
             double delta = vertrag.Rechnungsgrundlage.delta;
 
-            double lbw_gesamt = (lbw_tod + lbw_erleben * (1+delta));
-            double bbw_gesamt = bbw*(1-t* alpha/ lbw_praemie - beta-t*gamma* lbw_aufschubszeit/ lbw_praemie);
+            double lbw_gesamt = (lbw_tod + lbw_erleben * (1 + delta));
+            double bbw_gesamt = bbw * (1 - t * alpha / lbw_praemie - beta - t * gamma * lbw_aufschubszeit / lbw_praemie);
 
-            return lbw_gesamt/ bbw_gesamt;
+            return lbw_gesamt / bbw_gesamt;
         }
 
         public double BerechnZillmerbeitrag(Vertrag vertrag)
         {
-            double nettopraemie = BerechneNettobeitrag( vertrag);
-            double bruttopraemie = BerechneBruttobeitrag( vertrag);
+            double nettopraemie = BerechneNettobeitrag(vertrag);
+            double bruttopraemie = BerechneBruttobeitrag(vertrag);
             // Berechnungen bbw
             double bbw = GesamtOutput[0].lbw_praemie;
             int t = vertrag.Vertragsteil.t;
@@ -155,7 +169,7 @@ namespace Tarifrechner
             // Kostensaetze
             (double alpha_z1, double alpha_gamma1) = BerechneAbschlusskosten(vertrag);
 
-            double wert = nettopraemie + alpha_z1* bruttopraemie * t / bbw;
+            double wert = nettopraemie + alpha_z1 * bruttopraemie * t / bbw;
             return wert;
         }
 
@@ -171,8 +185,6 @@ namespace Tarifrechner
             //Gesamte Abschlusskoten
             double abKo = alpha_z1 * vertrag.Vertragsteil.t * bruttopraemie / GesamtOutput[0].lbw_praemie;
 
-
-
             for (int i = 0; i < vertrag.Vertragsteil.n; i++)
             {
                 // Berechnung fuer lbw
@@ -183,36 +195,83 @@ namespace Tarifrechner
                 //Barwerte
                 double bbw = GesamtOutput[i].lbw_praemie;
 
-                double mVx= lbw - nettopraemie * bbw;
+                double mVx = lbw - nettopraemie * bbw;
+                double mVx_z = mVx - abKo * bbw;
                 GesamtOutput[i].mVx = mVx;
-                GesamtOutput[i].mVx_z = mVx - abKo* bbw;
-                GesamtOutput[i].mVx_a = 0;
+                GesamtOutput[i].mVx_z = mVx_z;
+                if (i == 0)
+                {
+                    GesamtOutput[i].mVx_a = -vertrag.Rechnungsgrundlage.alpha * vertrag.Vertragsteil.t * bruttopraemie;
+                    GesamtOutput[i].mVx_a_gez = mVx_z;
+                }
+                else
+                {
+                    GesamtOutput[i].mVx_a = lbw - bbw * bruttopraemie;
+                    GesamtOutput[i].mVx_a_gez = lbw + GesamtOutput[i].gesamtkosten - bbw * bruttopraemie;
+                }
+
                 GesamtOutput[i].mVx_a_gez = 0;
                 GesamtOutput[i].mVx_B = 0;
             }
-
-
         }
 
-        public (double,double) BerechneAbschlusskosten(Vertrag vertrag)
+        public (double, double) BerechneAbschlusskosten(Vertrag vertrag)
         {
             double alpha_z = Math.Min(vertrag.Rechnungsgrundlage.alpha, 0.025);
-            double alpha_gamma = (vertrag.Rechnungsgrundlage.alpha - alpha_z) / GesamtOutput[0].lbw_praemie;
+            double alpha_gamma = (vertrag.Rechnungsgrundlage.alpha - alpha_z) / GesamtOutput[0].lbw_aufschubszeit;
             return (alpha_z, alpha_gamma);
+        }
+
+        public void BerechneKostenverlauf(Vertrag vertrag)
+        {
+            Vertragsteil vt = vertrag.Vertragsteil;
+            Rechnungsgrundlage rg = vertrag.Rechnungsgrundlage;
+            double nettopraemie = BerechneNettobeitrag(vertrag);
+            double bruttopraemie = BerechneBruttobeitrag(vertrag);
+
+            // Berechnungen bbw
+            int t = vertrag.Vertragsteil.t;
+
+            // Kostensaetze
+            (double alpha_z1, double alpha_gamma1) = BerechneAbschlusskosten(vertrag);
+            double beta = rg.beta;
+            double gamma = rg.gamma;
+            double delta = rg.delta;
+
+            double abKlo = bruttopraemie * vt.t * alpha_gamma1;
+
+            //Berechnung Kostenverlauf
+            GesamtOutput[vt.n].beta_kosten = 0;
+            GesamtOutput[vt.n].gamma_kosten = 0;
+            GesamtOutput[vt.n].delta_kosten = 0;
+            GesamtOutput[vt.n].alpha_gamma_kosten = 0;
+            GesamtOutput[vt.n].gesamtkosten = 0;
+            for (int i = vt.n - 1; i > -1; i--)
+            {
+                double bbw = GesamtOutput[i].lbw_praemie;
+                double lbw_erleben = GesamtOutput[i].lbw_erleben;
+                double lbw_aufschubszeit = GesamtOutput[i].lbw_aufschubszeit;
+                if (i < vt.t)
+                    GesamtOutput[i].beta_kosten = bbw * beta * bruttopraemie;
+                GesamtOutput[i].gamma_kosten = lbw_aufschubszeit * gamma * bruttopraemie * vt.t;
+                GesamtOutput[i].delta_kosten = lbw_erleben * delta;
+                GesamtOutput[i].alpha_gamma_kosten = abKlo * lbw_aufschubszeit;
+                GesamtOutput[i].gesamtkosten = GesamtOutput[i].beta_kosten + GesamtOutput[i].gamma_kosten + GesamtOutput[i].delta_kosten + GesamtOutput[i].alpha_gamma_kosten;
+            }
         }
         public void BerechneQx(Vertrag vertrag)
         {
             Vertragsteil vt = vertrag.Vertragsteil;
             Rechnungsgrundlage rg = vertrag.Rechnungsgrundlage;
             int index = 0;
-            for(int i = vt.ea; i< TafelListe.Count;i++) 
+            for (int i = vt.ea; i < TafelListe.Count; i++)
             {
                 Tafel zeile = TafelListe[i];
-                GesamtOutput wert=new();
+                GesamtOutput wert = new();
                 wert.qx = zeile.QX * rg.mannAnteil + (1 - rg.mannAnteil) * zeile.QY;
                 wert.m = index;
                 wert.alter = index + vt.ea;
-                wert.zins = rg.isKonstantZins? rg.zins : TafelListe[i- vt.ea].ZINS;
+                wert.zins = rg.isKonstantZins ? rg.zins : TafelListe[i - vt.ea].ZINS;
                 wert.aufzinsfaktor = wert.zins + 1;
                 wert.diskontierungsfaktor = 1 / wert.aufzinsfaktor;
                 index++;
@@ -220,7 +279,7 @@ namespace Tarifrechner
             }
 
             // Berechnung von LBW Tod
-            GesamtOutput[vt.n].lbw_tod= 0;
+            GesamtOutput[vt.n].lbw_tod = 0;
             for (int i = vt.n - 1; i > -1; i--)
             {
                 GesamtOutput[i].lbw_tod = (GesamtOutput[i + 1].lbw_tod * (1 - GesamtOutput[i].qx) + GesamtOutput[i].qx) * GesamtOutput[i].diskontierungsfaktor;
@@ -232,7 +291,7 @@ namespace Tarifrechner
                 double wertVorjahr = GesamtOutput[i + 1].lbw_erleben;
                 double px = (1 - GesamtOutput[i].qx);
                 double v = GesamtOutput[i].diskontierungsfaktor;
-                GesamtOutput[i].lbw_erleben = wertVorjahr * px * v ;
+                GesamtOutput[i].lbw_erleben = wertVorjahr * px * v;
             }
             //Berechnung von LBW Praemie
             GesamtOutput[vt.t].lbw_praemie = 0;
@@ -241,7 +300,7 @@ namespace Tarifrechner
                 double wertVorjahr = GesamtOutput[i + 1].lbw_praemie;
                 double px = (1 - GesamtOutput[i].qx);
                 double v = GesamtOutput[i].diskontierungsfaktor;
-                GesamtOutput[i].lbw_praemie = wertVorjahr * px  * v + 1;
+                GesamtOutput[i].lbw_praemie = wertVorjahr * px * v + 1;
             }
             //Berechnung von LBW Erlebensfall lbw_erleben
             GesamtOutput[vt.n].lbw_aufschubszeit = 0;
@@ -252,6 +311,7 @@ namespace Tarifrechner
                 double v = GesamtOutput[i].diskontierungsfaktor;
                 GesamtOutput[i].lbw_aufschubszeit = wertVorjahr * px * v + 1;
             }
+
         }
 
     }
